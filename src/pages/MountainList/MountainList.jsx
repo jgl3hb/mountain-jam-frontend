6918e5d898eea98318c7c75420856c82abc08d85
Mikhail <mishanyaveselyi@gmail.com>
2022-03-26 11:43:09 -0400

    Set functions to Show list of all moutnains

--- src/pages/MountainList/MountainList.jsx
+++ src/pages/MountainList/MountainList.jsx
@@ -1,23 +1,36 @@
 import { useState, useEffect } from 'react'
 import * as mountainService from '../../services/mountainService'
 
-const Mountains = () => {
+const MountainList = () => {
   const [mountains, setMountains] = useState([])
 
   useEffect(()=> {
     mountainService.getAllMountains()
     .then(mountains => setMountains(mountains))
   }, [])
+    return (
+      <>
+      <main>
 
-}
-
-const MountainList = (props) => {
-  return (
-    <>
       <h1>Mountain List</h1>
+      <div className='cards'>
 
-    </>
-  )
+      {mountains.length ? 
+        <>
+          {mountains.map(mountain=>
+            <p className='each-card' key={mountain._id}>{mountain.name}</p>
+            )}
+        </>
+        :
+        <p>No mountains yet</p>
+      }
+      </div>
+
+      </main>
+      </>
+    )
+
 }
+
 
 export default MountainList;
