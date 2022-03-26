87e54b4d5a5f6ad6c63c51b4eb35bb30d2655a1f
Mikhail <mishanyaveselyi@gmail.com>
2022-03-26 10:49:52 -0400

    Create mountain Service, import mountainService to Mountain List

--- src/pages/MountainList/MountainList.jsx
+++ src/pages/MountainList/MountainList.jsx
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react'
+import * as mountainService from '../../services/mountainService'
 
+const Mountains = () => {
+  const [mountains, setMountains] = useState([])
+
+  useEffect(()=> {
+    mountainService.getAllMountains()
+    .then(mountains => setMountains(mountains))
+  }, [])
+
+}
 
 const MountainList = (props) => {
   return (
     <>
       <h1>Mountain List</h1>
+
     </>
   )
 }
