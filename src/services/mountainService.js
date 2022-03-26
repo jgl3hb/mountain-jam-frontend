2f8e0f9ec037bbd7af21fb3b7e99dddd2afaddde
Mikhail <mishanyaveselyi@gmail.com>
2022-03-26 11:08:41 -0400

    Set functions for mountainService

---
+++ src/services/mountainService.js
@@ -0,0 +1,10 @@
+// import * as tokenService from '../services/tokenService'
+
+const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/mountains`
+
+async function getAllMountains() {
+  const res = await fetch(BASE_URL)
+  return await res.json()
+}
+
+export { getAllMountains }
