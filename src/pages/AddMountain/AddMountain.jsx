9c3bae7425dce2c7088a1ba0080c06a7a542aece
jgl3hb <lewis.jeffreyg@gmail.com>
2022-03-27 13:26:12 -0400

    addmt function

---
+++ src/pages/AddMountain/AddMountain.jsx
@@ -0,0 +1,15 @@
+import { useState, useRef, useEffect } from "react"
+
+function AddMountain(props) {
+  return (
+    <>
+      <main>
+        <h3>Add mountain</h3>
+
+      </main>
+    
+    </>
+  )
+}
+
+export default AddMountain
