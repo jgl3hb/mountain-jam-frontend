ea3ac5f24d5ea82cfbe351c55245f2536fd0aee2
jgl3hb <lewis.jeffreyg@gmail.com>
2022-03-27 14:07:38 -0400

    mt input

--- src/pages/AddMountain/AddMountain.jsx
+++ src/pages/AddMountain/AddMountain.jsx
@@ -1,10 +1,58 @@
 import { useState, useRef, useEffect } from "react"
 
 function AddMountain(props) {
+  const formElement = useRef()
+  const [validForm, setValidForm] = useState (false)
+  const [formData, setFormData] = useState({
+    name: '',
+    elevation: 0,
+    country: '',
+  })
+
+  useEffect(()=> {
+    formElement.current.checkValidity()?
+    setValidForm(true) : setValidForm(false)
+  }, [formData])
+
+  const handleChange = evt => {
+    setFormData({...formData, [evt.target.name]: evt.target.value})
+  }
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+		const mountainFormData = new FormData()
+		// mountainFormData.append('photo', formData.photo)
+    mountainFormData.append('name', formData.name)
+    mountainFormData.append('elevation', formData.elevation)
+    mountainFormData.append('country', formData.country)
+    props.handleAddMountain(mountainFormData)
+  }
+
+  // const handleChangePhoto = evt => {
+	// 	setFormData({...formData, photo: evt.target.files[0]})
+	// }
+
+
   return (
     <>
       <main>
         <h3>Add mountain</h3>
+        <form autoComplete="off" ref={formElement} onSubmit={handleSubmit}>
+        <div>
+        <label htmlFor="name-input">
+						Mountain's Name (required)
+				</label>
+        <br />
+        <input 
+						type="text"
+						id="name-input"
+						name="name"
+            value={formData.name}
+            onChange={handleChange}
+						required
+					/>
+        </div>
+        </form>
 
       </main>
     
