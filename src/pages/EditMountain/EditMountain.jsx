75617f92c23cae3b85f4dcd1c840047f54fb52fe
jgl3hb <lewis.jeffreyg@gmail.com>
2022-03-28 10:41:52 -0400

    edtmtn page

---
+++ src/pages/EditMountain/EditMountain.jsx
@@ -0,0 +1,25 @@
+import { useState, useRef, useEffect } from "react"
+import { Link, useLocation } from 'react-router-dom'
+
+function EditMountain(props) {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default EditMountain
