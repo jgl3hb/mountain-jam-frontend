67c1311d1d35efd3aab8650564e9fb64d8d2627b
Mikhail <mishanyaveselyi@gmail.com>
2022-03-28 09:30:33 -0400

    Move Add Mountain button to My Profile page

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
+
 const MyProfile = (props) => {
   return (
     <>
     <main>
       <h1>Profile Details</h1>
       <p>This is {props.user.name}'s page</p>
+      <Link
+        to='/addmountain'
+      >
+        <button>
+          Add Mountain
+        </button>
+      </Link>
     </main>
     </>
   )
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -6,13 +6,7 @@ const ProfileDetails = (props) => {
     <main>
       <h1>Profile Details</h1>
       <p>This is {props.profile.name}'s page</p>
-      <Link
-        to='/addmountain'
-      >
-        <button>
-          Add Mountain
-        </button>
-      </Link>
+      
       
     </main>
     </>
