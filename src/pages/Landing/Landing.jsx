d4aa67a67b98e7eab6fd46d1e748de4ee3e48a0a
Mikhail <mishanyaveselyi@gmail.com>
2022-03-29 19:51:38 -0400

    Landing page background change

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,12 +2,13 @@ import styles from './Landing.module.css'
 
 const Landing = ({ user }) => {
   return (
-    <main className={styles.container}>
-      <h1>Welcome to Mountain Jam, {user ? user.name : 'climber'} !</h1>
-      <p id='quote'>
-      “Climb the mountains and get their good tidings. Nature's peace will flow into you as sunshine flows into trees. The winds will blow their own freshness into you, and the storms their energy, while cares will drop away from you like the leaves of Autumn.”
-      </p>
-    </main>
+
+      <main className={styles.container}>
+        <h1>Welcome to Mountain Jam, {user ? user.name : 'climber'} !</h1>
+        <p id='quote'>
+        “Climb the mountains and get their good tidings. Nature's peace will flow into you as sunshine flows into trees. The winds will blow their own freshness into you, and the storms their energy, while cares will drop away from you like the leaves of Autumn.”
+        </p>
+      </main>
   )
 }
 
