220bd9f5d2d9f59059e88afa31a55fd2912d288f
jgl3hb <Lewis.jeffreyg@gmail.com>
2022-03-29 20:01:35 -0400

    Update Landing.jsx

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -6,7 +6,7 @@ const Landing = ({ user }) => {
       <main className={styles.container}>
         <h1>Welcome to Mountain Jam, {user ? user.name : 'climber'} !</h1>
         <p id='quote'>
-        “Climb the mountains and get their good tidings. Nature's peace will flow into you as sunshine flows into trees. The winds will blow their own freshness into you, and the storms their energy, while cares will drop away from you like the leaves of Autumn.”
+        “Climb the mountains and get their good tidings. Nature's peace will flow into you as sunshine flows into trees. The winds will blow their own freshness into you, and the storms their energy, while cares will drop away from you like the leaves of Autumn.” - John Muir
         </p>
       </main>
   )
