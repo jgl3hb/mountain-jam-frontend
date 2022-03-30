b1ae9f06a7fa97e56f83f27ea080f55a4a314d1e
Mikhail <mishanyaveselyi@gmail.com>
2022-03-30 15:59:39 -0400

    Show Mountains in cards in profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,15 +1,34 @@
 import { Link } from "react-router-dom";
 import MyProfile from "../../components/MyProfile/MyProfile";
+import Mountain from "../../components/Mountain/Mountain";
 
 const ProfileDetails = (props) => {
-  console.log('user', props)
+  const toVisit = props.profile.toVisit
   return (
     <>
     <main>
-      <h1>Profile Details</h1>
+      <h1>Profile Details!!!</h1>
         {!props.userProfile ? 
         <>
           <p>This is {props.profile.name}'s page</p>
+          <div>My Mountains</div>
+          <div className='cards'>
+            {toVisit.length ? 
+          <>
+            {toVisit.map(mountain=>
+              <Mountain 
+              key={mountain._id}
+              mountain={mountain}
+              handleDeleteMountain={props.handleDeleteMountain}
+              user={props.user}/>
+            )}
+        
+        </>
+        :
+        <p>No mountains yet</p>
+      }
+      </div>
+
           {props.profile._id === props.user.profile ? 
             <MyProfile />
           :
