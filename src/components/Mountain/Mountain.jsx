f7027783b0895104c58ca13a3f127fa87035aa91
jgl3hb <lewis.jeffreyg@gmail.com>
2022-03-31 10:53:43 -0400

    photo in card

--- src/components/Mountain/Mountain.jsx
+++ src/components/Mountain/Mountain.jsx
@@ -6,15 +6,10 @@ const Mountain = ({mountain}) => {
   // console.log("props", mountain)
   return (
     <>
-      <div key={mountain._id} className='each-card'
-      img 
-      src={mountain.photo ? mountain.photo : `https://picsum.photos/id/1018/200/150`} 
-      className="card-img-top" 
+      <div 
+      style={mountain.photo ? mountain.photo : `https://picsum.photos/id/1018/200/150`}
+      key={mountain._id} className='each-card' 
       >
-      <img 
-        src={mountain.photo ? mountain.photo : `https://picsum.photos/id/1018/200/150`} 
-        className="card-img-top" 
-      />
         <Link state={{mountain}} to="/mountain">
           <p >{mountain.name}</p>
         </Link>
