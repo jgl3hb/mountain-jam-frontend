1199d9e1e0b5e6c4da574a97b63215654b6eb1ce
Mikhail <mishanyaveselyi@gmail.com>
2022-04-01 01:19:33 -0400

    Mountain cards hover effect

--- src/components/Mountain/Mountain.jsx
+++ src/components/Mountain/Mountain.jsx
@@ -4,13 +4,12 @@ import { Link } from "react-router-dom";
 const Mountain = ({mountain}) => {
   return (
     <>
-      <div key={mountain._id} className='each-card-top'
+      <div 
+      style={{  
+        backgroundImage: `url(${mountain.photo? mountain.photo : `https://picsum.photos/id/1018/200/150`})`}
+      }
+      key={mountain._id} className='each-card-top'
       >
-      <img className="mountain-pic"
-        src={mountain.photo ? mountain.photo : `https://picsum.photos/id/1018/200/150`} 
-        // className="card-img-top" 
-        alt='mountain'
-      />
         <Link state={{mountain}} to="/mountain">
           <p className="mntn-name">{mountain.name}</p>
         </Link>
